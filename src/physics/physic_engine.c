96365a2e672d74722a0a36e37e4819e830ef89f6
Volker Kuntner <volker.kuntner@tutanota.com>
2025-06-01 11:51:28 +0200

    change update cell order from top to bottom to the oppesite

--- src/physics/physic_engine.c
+++ src/physics/physic_engine.c
@@ -5,7 +5,8 @@
 void cal_physic_cell_matrix(const CellMatrix read_matrix,
                             CellMatrix *p_write_matrix) {
   for (uint32_t x = 0; x < GRID_WIDTH; x++) {
-    for (uint32_t y = 0; y < GRID_HEIGHT; y++) {
+    for (uint32_t y = GRID_WIDTH - 1; y > 0; y--) {
+      // for (uint32_t y = 0; y < GRID_HEIGHT; y++) {
 
       // Find non-empty cell in cell matrix
       if (read_matrix[x][y].cell_id != EMPTY) {
