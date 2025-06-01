6159a01b5c62150f56f590e9d6928a492237a675
Volker Kuntner <volker.kuntner@tutanota.com>
2025-06-01 12:19:18 +0200

    refactor mouse handler

--- src/input/mouse_handler.c
+++ src/input/mouse_handler.c
@@ -1,33 +1,36 @@
 #include "mouse_handler.h"
 
-void handle_mouse_events(SDL_Event e, CellMatrix *render_grid) {
+void handle_mouse_events(SDL_Event e, CellMatrix *p_render_matrix) {
+
+  switch (e.type) {
 
   // Handle mouse click
-  if (e.type == SDL_MOUSEBUTTONDOWN) {
+  case SDL_MOUSEBUTTONDOWN:
     if (e.button.button == SDL_BUTTON_LEFT) {
-      int mouseX, mouseY;
-      SDL_GetMouseState(&mouseX, &mouseY);
-      handle_mouse_click(render_grid, mouseX, mouseY);
+      set_cell(p_render_matrix, e.motion.x, e.motion.y);
     }
-  }
+    break;
+
   // Handle mouse motion while button is pressed
-  else if (e.type == SDL_MOUSEMOTION) {
+  case SDL_MOUSEMOTION:
     if (e.motion.state & SDL_BUTTON_LMASK) {
-      int mouseX = e.motion.x;
-      int mouseY = e.motion.y;
-      handle_mouse_click(render_grid, mouseX, mouseY);
+      set_cell(p_render_matrix, e.motion.x, e.motion.y);
     }
+    break;
+
+  default:
+    break;
   }
 }
 
-void handle_mouse_click(CellMatrix *grid, int mouseX, int mouseY) {
-  // Convert screen coordinates to grid coordinates
-  int gridX = mouseX / CELL_LENGTH;
-  int gridY = mouseY / CELL_LENGTH;
+void set_cell(CellMatrix *p_render_matrix, int mouseX, int mouseY) {
+  // Convert screen coordinates to matrix coordinates
+  uint32_t gridX = mouseX / CELL_LENGTH;
+  uint32_t gridY = mouseY / CELL_LENGTH;
 
   // Ensure coordinates are within bounds
-  if (gridX >= 0 && gridX < MATRIX_WIDTH && gridY >= 0 &&
+  if (gridX - 1 > 0 && gridX < MATRIX_WIDTH && gridY > 0 &&
       gridY < MATRIX_HEIGHT) {
-    (*grid)[gridX][gridY].cell_id = SAND;
+    (*p_render_matrix)[gridX][gridY].cell_id = SAND;
   }
 }
--- src/input/mouse_handler.h
+++ src/input/mouse_handler.h
@@ -5,6 +5,6 @@
 #include "../../includes/cell_types.h"
 #include "../../includes/sdl_utils.h"
 
-void handle_mouse_events(SDL_Event e, CellMatrix *p_render_cell_matrix);
-void handle_mouse_click(CellMatrix *p_cell_matrix, int mouseX, int mouseY);
+void handle_mouse_events(SDL_Event e, CellMatrix *p_render_matrix);
+void set_cell(CellMatrix *p_cell_matrix, int mouseX, int mouseY);
 #endif //  MOUSE_HANDLER_H
